5bd7a2b8feafce62e124baea761db7ea7eeb6c2c
Derek Day <derek.andrew.day@gmail.com>
2025-06-03 01:50:07 -0400

    a

--- app/page.js
+++ app/page.js
@@ -86,28 +86,28 @@ export default function Home() {
               animate="visible"
               exit="exit"
             >
-              <h1 className="text-2xl font-bold mb-4 capitalize">{authMode}</h1>
+              <h1 className="text-2xl tracking-wider mb-4 capitalize text-white">{authMode}</h1>
               <input
                 type="email"
                 placeholder="Email"
-                className="w-full p-2 border border-gray-300 rounded mb-3"
+                className="w-full p-2 border mb-3"
                 onChange={(e) => setEmail(e.target.value)}
               />
               <input
                 type="password"
                 placeholder="Password"
-                className="w-full p-2 border border-gray-300 rounded mb-3"
+                className="w-full p-2 border mb-3"
                 onChange={(e) => setPassword(e.target.value)}
               />
               <button
                 onClick={handleSubmit}
-                className="w-full bg-blue-600 text-white py-2 rounded mb-4 hover:bg-blue-700"
+                className="login text-2xl tracking-wide w-full text-white py-3 mb-4"
               >
                 Submit
               </button>
               <button
                 onClick={() => setAuthMode(null)}
-                className="text-sm text-gray-600 hover:underline"
+                className="text-sm text-gray-600 hover:underline text-white"
               >
                 Back
               </button>
